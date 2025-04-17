38f70b45e1af054006d851b5db59322d2299185a
panoscure <panoscure@gmail.com>
2025-04-17 15:07:04 +0300

    Api Support Example Commit

--- src/test/java/runners/TestApiRunner.java
+++ src/test/java/runners/TestApiRunner.java
@@ -3,6 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
+
 @CucumberOptions(
         features = {
                 "src/test/resources/features/api.feature"
